c440269889706e72367cc641ea10fccaec5b43f1
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-01-17 14:04:24 +0530

    adding division.java

--- division.java
+++ division.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class division {
+import java.util.Scanner;
+class division {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Output: " + (num1 / num2));
+    }
 }
