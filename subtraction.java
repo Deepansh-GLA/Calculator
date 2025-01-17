1bb227c70d023710e463f751b63bd1f08a80d7be
Deepansh-GLA <deepansh.niranjan_cs21@gla.ac.in>
2025-01-17 13:51:01 +0530

    adding subtraction.java

--- subtraction.java
+++ subtraction.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
-public class subtraction {
+import java.util.Scanner;
+class subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+        System.out.println("Output: " + (num1 - num2));
+    }
 }
